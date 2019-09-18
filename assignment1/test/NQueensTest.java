7345b5065599a56421b04483747989423261b358
revsic <revsic99@gmail.com>
2019-09-18 13:03:56 +0900

    nqueenstest: Add test for NQueens

---
+++ assignment1/test/NQueensTest.java
@@ -0,0 +1,47 @@
+package assignment1.test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import assignment1.NQueens;
+
+
+public class NQueensTest implements Testable {
+    @Override
+    public boolean test() {
+        if (!positionSetterGetterTest()) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean positionSetterGetterTest() {
+        NQueens nqueens = new NQueens(4);
+        
+        nqueens.setPos(1, 1);
+        nqueens.setPos(3, 2);
+
+        int[][] positionList = {
+            { 1, 1 },
+            { 3, 2 }
+        };
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                final int[] pos = { i, j };
+                boolean value = nqueens.getPos(i, j);
+                boolean existance =
+                    Stream.of(positionList)
+                          .anyMatch(x -> Arrays.equals(pos, x));
+                if (value != existance) {
+                    System.out.println("setPos, getPos failure: i=" + i + 
+                                       ", j=" + j + ", value=" + value + 
+                                       ", existance=" + existance);
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    } 
+}
