e13a59c7411e9d7ac82dd15b159a485968074f91
revsic <revsic99@gmail.com>
2019-09-18 23:14:14 +0900

    assignment1: Add simple application

--- assignment1/Application.java
+++ assignment1/Application.java
@@ -1,7 +1,17 @@
 package assignment1;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import assignment1.algorithms.DFS;
+
 
 public class Application {
     public static void main(String[] args) {
+        int[][] res = new DFS().solve(4);
+        String rows = Stream.of(res)
+            .map(x -> String.valueOf(x[1]))
+            .collect(Collectors.joining(" "));
+
+        System.out.println(rows);
     }
 }
