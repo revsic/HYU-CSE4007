828a959be191d9589bdaf7b524d8a26d7d658fdd
revsic <revsic99@gmail.com>
2019-09-18 23:28:38 +0900

    application: Split to method solve

--- assignment1/Application.java
+++ assignment1/Application.java
@@ -1,17 +1,33 @@
 package assignment1;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import assignment1.algorithms.DFS;
+import assignment1.algorithms.Solution;
 
 
 public class Application {
-    public static void main(String[] args) {
-        int[][] res = new DFS().solve(4);
-        String rows = Stream.of(res)
+    public static String solve(Solution solution, int size) {
+        int[][] res = solution.solve(size);
+        if (res == null) {
+            return "No solution";
+        }
+
+        Arrays.sort(res, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] c1, int[] c2) {
+                return c1[0] - c2[0];
+            }
+        });
+
+        return Stream.of(res)
             .map(x -> String.valueOf(x[1]))
             .collect(Collectors.joining(" "));
+    }
 
-        System.out.println(rows);
+    public static void main(String[] args) {
+        System.out.println(solve(new DFS(), 5));
     }
 }
