8bc8aa6090ae056545dee541a35d2ace4cc964a3
revsic <revsic99@gmail.com>
2019-09-18 22:15:37 +0900

    nqueens test: Add isSolvedTest

--- assignment1/test/NQueensTest.java
+++ assignment1/test/NQueensTest.java
@@ -14,25 +14,18 @@ public class NQueensTest implements Testable {
             && isSolvedTest();
     }
 
-    private boolean positionSetterGetterTest() {
-        int[][] positionList = { { 1, 1 }, { 3, 2 } };
-
-        NQueens nqueens = new NQueens(4);
-        for (int i = 0; i < positionList.length; ++i) {
-            int[] pos = positionList[i];
-            nqueens.setPos(pos[0], pos[1]);
-        }
-
+    private boolean positionTest(int[][] posList, NQueens queens, String msgbase) {
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
                 final int[] pos = { i, j };
-                boolean value = nqueens.getPos(i, j);
-                boolean existance =
-                    Stream.of(positionList)
-                          .anyMatch(x -> Arrays.equals(pos, x));
+                boolean value = queens.getPos(i, j);
+                boolean existance = Stream.of(posList)
+                                          .anyMatch(x -> Arrays.equals(pos, x));
                 if (value != existance) {
-                    System.out.println("setPos, getPos failure: i=" + i + 
-                                       ", j=" + j + ", value=" + value + 
+                    System.out.println(msgbase + 
+                                       ": i=" + i + 
+                                       ", j=" + j +
+                                       ", value=" + value + 
                                        ", existance=" + existance);
                     return false;
                 }
@@ -40,6 +33,18 @@ public class NQueensTest implements Testable {
         }
 
         return true;
+    }
+
+    private boolean positionSetterGetterTest() {
+        int[][] positionList = { { 1, 1 }, { 3, 2 } };
+
+        NQueens nQueens = new NQueens(4);
+        for (int i = 0; i < positionList.length; ++i) {
+            int[] pos = positionList[i];
+            nQueens.setPos(pos[0], pos[1]);
+        }
+
+        return positionTest(positionList, nQueens, "setPos, getPos failure");
     }
 
     private boolean cloneTest() {
@@ -52,23 +57,48 @@ public class NQueensTest implements Testable {
         }
 
         NQueens cloned = nQueens.clone();
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                final int[] pos = { i, j };
-                boolean value = cloned.getPos(i, j);
-                boolean existance = Stream.of(positionList)
-                                          .anyMatch(x -> Arrays.equals(pos, x));
-                if (value != existance) {
-                    System.out.println("clone failure");
-                    return false;
-                }
-            }
-        }
+        return positionTest(positionList, cloned, "clone failure");
+    }
 
-        return true;
+    private boolean caseTest(int[][] coords) {
+        NQueens nQueens = new NQueens(4);
+        for (int i = 0; i < coords.length; ++i) {
+            int[] pos = coords[i];
+            nQueens.setPos(pos[0], pos[1]);
+        } 
+        return nQueens.isSolved();
     }
 
     private boolean isSolvedTest() {
+        int[][][] failed = {
+            { { 0, 0 }, { 1, 3 }, { 2, 0 }, { 3, 2 } }, // 1st row
+            { { 0, 0 }, { 1, 3 }, { 2, 3 }, { 3, 1 } }, // 4th row
+            { { 0, 0 }, { 3, 1 }, { 0, 2 }, { 2, 3 } }, // 1st column
+            { { 2, 0 }, { 0, 1 }, { 2, 2 }, { 1, 3 } }, // 3rd column + lowerhalf leftdown
+            { { 0, 1 }, { 1, 2 }, { 2, 2 }, { 3, 0 } }, // 3rd row + lowerhalf rightdown
+            { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 3, 3 } }, // lowerhalf rightdown
+            { { 0, 2 }, { 1, 0 }, { 2, 1 }, { 3, 3 } }, // upperhalf rightdown
+            { { 0, 3 }, { 1, 1 }, { 2, 0 }, { 3, 2 } }, // upperhalf leftdown
+        };
+
+        int[][][] success = {
+            { { 0, 1 }, { 1, 3 }, { 2, 0 }, { 3, 2 } },
+            { { 0, 2 }, { 1, 0 }, { 2, 3 }, { 3, 1 } },
+        };
+
+        for (int i = 0; i < failed.length; ++i) {
+            if (caseTest(failed[i])) {
+                System.out.println("isSolved failure: false case, " + failed[i]);
+                return false;
+            }
+        }
+
+        for (int i = 0; i < success.length; ++i) {
+            if (!caseTest(success[i])) {
+                System.out.println("isSolved failure: true case, " + success[i]);
+            }
+        }
+
         return true;
     }
 }
