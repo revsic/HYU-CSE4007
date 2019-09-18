7bed8647109f605ec327f5e67b026758ee9ae56c
revsic <revsic99@gmail.com>
2019-09-18 22:33:54 +0900

    dfs: Add DFS Solution template

---
+++ assignment1/algorithms/DFS.java
@@ -0,0 +1,10 @@
+package assignment1.algorithms;
+
+import assignment1.problem.NQueens;
+
+
+public class DFS implements Solution {
+    public int[][] solve(NQueens nQueens) {
+        return null;
+    }
+}
