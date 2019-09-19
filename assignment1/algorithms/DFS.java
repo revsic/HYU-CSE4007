aabb973bfad5255cb5440640b39a77615c33fdaf
revsic <revsic99@gmail.com>
2019-09-19 17:28:12 +0900

    dfs: Add max depth

--- assignment1/algorithms/DFS.java
+++ assignment1/algorithms/DFS.java
@@ -5,13 +5,25 @@ import assignment1.algorithms.Record;
 
 
 public class DFS implements Solution {
+    private int maxdepth;
+
+    public DFS() {
+        this.maxdepth = Integer.MAX_VALUE;
+    }
+
+    public DFS(int maxdepth) {
+        this.maxdepth = maxdepth;
+    }
+
     public int[][] solve(int size) {
         Stack<Record> stack = new Stack<Record>();
         stack.push(Record.empty(size));
 
         while (!stack.empty()) {
             Record record = stack.pop();
-            if (record.coords.size() == size) {
+            if (record.coords.size() > maxdepth
+                || record.coords.size() == size)
+            {
                 if (record.nQueens.isSolved()) {
                     return record.coords.toArray(new int[2][size]);
                 } else {
