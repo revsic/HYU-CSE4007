ee77ebbaeace62305ffc29172a26ffffd92b1045
revsic <revsic99@gmail.com>
2019-09-19 16:51:08 +0900

    dfs: Fix bug

--- assignment1/algorithms/DFS.java
+++ assignment1/algorithms/DFS.java
@@ -8,16 +8,21 @@ import assignment1.problem.NQueens;
 public class DFS implements Solution {
     public int[][] solve(int size) {
         Stack<Record> stack = new Stack<Record>();
+        stack.push(Record.empty(size));
 
         while (!stack.empty()) {
             Record record = stack.pop();
-            if (record.coords.size() == size && record.nQueens.isSolved()) {
-                return record.coords.toArray(new int[2][size]);
+            if (record.coords.size() == size) {
+                if (record.nQueens.isSolved()) {
+                    return record.coords.toArray(new int[2][size]);
+                } else {
+                    continue;
+                }
             }
 
             int row = record.coords.size();
             for (int i = 0; i < size; ++i) {
-                stack.push(record.addCoord(i, row));
+                stack.push(record.addCoord(row, i));
             }
         }
 
