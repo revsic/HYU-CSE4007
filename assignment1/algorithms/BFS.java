3512d446cf10b8b6c10b7846c14a579427950da5
revsic <revsic99@gmail.com>
2019-09-21 17:44:23 +0900

    bfs: Add annotation

--- assignment1/algorithms/BFS.java
+++ assignment1/algorithms/BFS.java
@@ -3,11 +3,20 @@ package assignment1.algorithms;
 import java.util.ArrayDeque;
 
 
+/**
+ * N-Queens problem solver with breadth-first search.
+ */
 public class BFS implements Solution {
+    /**
+     * Solve N-Queens problem.
+     * @param size int, size of the board.
+     * @return 2D coordinates array for queens if solution exist, else null.
+     */
     public int[][] solve(int size) {
         ArrayDeque<Record> queue = new ArrayDeque<Record>();
         queue.push(Record.empty(size));
 
+        // queue base iteration
         while (queue.size() > 0) {
             Record record = queue.removeFirst();
             if (record.coords.size() == size) {
@@ -18,6 +27,7 @@ public class BFS implements Solution {
                 }
             }
 
+            // expanding tree
             int row = record.coords.size();
             for (int i = 0; i < size; ++i) {
                 queue.add(record.addCoord(row, i));
