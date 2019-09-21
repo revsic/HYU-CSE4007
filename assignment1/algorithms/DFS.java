ca0829fb830e4f6b470a74951d2f734166db1e04
revsic <revsic99@gmail.com>
2019-09-21 17:34:29 +0900

    dfs: Add documentation

--- assignment1/algorithms/DFS.java
+++ assignment1/algorithms/DFS.java
@@ -3,23 +3,41 @@ package assignment1.algorithms;
 import java.util.Stack;
 
 
+/**
+ * N-Queens solution with depth-first search.
+ */
 public class DFS implements Solution {
     private int maxdepth;
 
+    /**
+     * Construct new DFS solution.
+     */
     public DFS() {
         this.maxdepth = Integer.MAX_VALUE;
     }
 
+    /**
+     * Construct DFS solution with limited depth.
+     * @param maxdepth int, max depth in searching phase.
+     */
     public DFS(int maxdepth) {
         this.maxdepth = maxdepth;
     }
 
+    /**
+     * Solve the N-Queens problem.
+     * @param size int, size of the board.
+     * @return 2D coordinates array for queens if solution exist, else null.
+     */
+    @Override
     public int[][] solve(int size) {
         Stack<Record> stack = new Stack<Record>();
         stack.push(Record.empty(size));
 
+        // stack base iteration
         while (!stack.empty()) {
             Record record = stack.pop();
+            // depth limited searching
             if (record.coords.size() > maxdepth
                 || record.coords.size() == size)
             {
@@ -30,6 +48,7 @@ public class DFS implements Solution {
                 }
             }
 
+            // expanding tree
             int row = record.coords.size();
             for (int i = 0; i < size; ++i) {
                 stack.push(record.addCoord(row, i));
