716b92f686f1339f02d22a75a91bb4b7231d21bf
revsic <revsic99@gmail.com>
2019-09-21 17:36:05 +0900

    dfid: Add annotation

--- assignment1/algorithms/DFID.java
+++ assignment1/algorithms/DFID.java
@@ -1,12 +1,24 @@
 package assignment1.algorithms;
 
 
+/**
+ * N-Queens solver with DFS and iterative deepening.
+ */
 public class DFID implements Solution {
+    /**
+     * Solve N-Queens problem.
+     * @param size size of the board.
+     * @return 2D coordinates array for queens if solution exist, else null. 
+     */
+    @Override
     public int[][] solve(int size) {
         for (int i = 1; i <= size; ++i) {
+            // limiting depth
             Solution dfs = new DFS(i);
 
+            // try to solve
             int[][] res = dfs.solve(size);
+            // if solution exist
             if (res != null) {
                 return res;
             }
