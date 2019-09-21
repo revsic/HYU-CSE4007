79303637e2beb69143cd2bb9741bece264f133bd
revsic <revsic99@gmail.com>
2019-09-21 22:45:11 +0900

    testable: Add documentation

--- assignment1/test/Testable.java
+++ assignment1/test/Testable.java
@@ -1,6 +1,13 @@
 package assignment1.test;
 
 
+/**
+ * Interface for testable object.
+ */
 public interface Testable {
+    /**
+     * Run test and return whether success or failure.
+     * @return bool value, whether success or failure.
+     */
     public boolean test();
 }
