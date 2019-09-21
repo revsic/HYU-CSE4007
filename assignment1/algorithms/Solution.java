556723d53a4414206d54b7387a4151f5a8b48aed
revsic <revsic99@gmail.com>
2019-09-21 17:17:19 +0900

    solution: Add annotation

--- assignment1/algorithms/Solution.java
+++ assignment1/algorithms/Solution.java
@@ -1,6 +1,14 @@
 package assignment1.algorithms;
 
 
+/**
+ * N-Queens solutions for application.
+ */
 public interface Solution {
+    /**
+     * Solve the N-Queens problem.
+     * @param size size of the board.
+     * @return 2D coordinates array for placing queens.
+     */
     public int[][] solve(int size);
 }
