53e250b717936723d8316b9cd8259289e05fd72f
revsic <revsic99@gmail.com>
2019-09-21 22:46:02 +0900

    cpplication: Add documentation

--- assignment1/test/Application.java
+++ assignment1/test/Application.java
@@ -1,7 +1,14 @@
 package assignment1.test;
 
 
+/**
+ * Application for unit test.
+ */
 public class Application {
+    /**
+     * Run unittest and show result.
+     * @param args @deprecated not used.
+     */
     public static void main(String[] args) {
         NQueensTest nQueensTest = new NQueensTest();
         if (!nQueensTest.test()) {
