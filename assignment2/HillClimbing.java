790e52bd38b1793327c1c1d45267164aa4952a4f
revsic <revsic99@gmail.com>
2019-10-01 17:03:33 +0900

    hill climbing: Add annotation

--- assignment2/HillClimbing.java
+++ assignment2/HillClimbing.java
@@ -3,12 +3,24 @@ package assignment2;
 import problem.nqueens.Solution;
 
 
+/**
+ * N-Queens problem solver with Hill-Climbing policy.
+ */
 public class HillClimbing implements Solution {
+    /**
+     * Name of the solution.
+     * @return name of the solution.
+     */
     @Override
     public String name() {
         return "Hill Climbing";
     }
 
+    /**
+     * Solve N-Queens problem.
+     * @param size int, size of the board.
+     * @return 2D coordinates array for queens if solution exist, else null.
+     */
     @Override
     public int[][] solve(int size) {
         return null;
