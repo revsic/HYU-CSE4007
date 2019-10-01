b5c5933cfac861d726d654fb26174853d23e5fbe
revsic <revsic99@gmail.com>
2019-10-01 17:01:23 +0900

    app: Add application for assignment2

---
+++ assignment2/Application.java
@@ -0,0 +1,26 @@
+package assignment2;
+
+import problem.nqueens.App;
+
+
+/**
+ * Application for showing assignment1 solutions of N-Queens problem.
+ */
+public class Application {
+    /**
+     * Main application, run BFS, DFS, DFID with given board size and log path.
+     * @param args command line argument, board size and log path.
+     * @exception java.lang.NumberFormatException exception can occur in Integer.parseInt.
+     */
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("usage: [size of board: INT] [path: STRING]");
+            return;
+        }
+
+        int size = Integer.parseInt(args[0]);
+        String path = args[1];
+
+        App.run(new HillClimbing(), size, path);
+    }
+}
