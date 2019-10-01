f63b98441fbe3a5771c9a3e7859e471ea80bd772
revsic <revsic99@gmail.com>
2019-10-01 16:17:11 +0900

    dfid: Fix solution

--- assignment1/algorithms/DFID.java
+++ assignment1/algorithms/DFID.java
@@ -1,10 +1,21 @@
 package assignment1.algorithms;
 
+import problem.nqueens.Solution;
+
 
 /**
  * N-Queens solver with DFS and iterative deepening.
  */
 public class DFID implements Solution {
+    /**
+     * Name of the solution.
+     * @return name of the solution.
+     */
+    @Override
+    public String name() {
+        return "DFID";
+    }
+
     /**
      * Solve N-Queens problem.
      * @param size size of the board.
