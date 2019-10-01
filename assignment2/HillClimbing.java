8ee9bee31f336d48b339f665003f223cbe188879
revsic <revsic99@gmail.com>
2019-10-01 17:01:15 +0900

    hill climbing: Add hill climbing method

---
+++ assignment2/HillClimbing.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+import problem.nqueens.Solution;
+
+
+public class HillClimbing implements Solution {
+    @Override
+    public String name() {
+        return "Hill Climbing";
+    }
+
+    @Override
+    public int[][] solve(int size) {
+        return null;
+    }
+}
