b42d3b4640fe6883f69972f79202c96e787c474f
revsic <revsic99@gmail.com>
2019-10-01 15:35:13 +0900

    solution: Move to outer problem

--- problem/nqueens/Solution.java
+++ problem/nqueens/Solution.java
@@ -1,10 +1,15 @@
-package assignment1.algorithms;
-
+package problem.nqueens;
 
 /**
  * N-Queens solutions for application.
  */
 public interface Solution {
+    /**
+     * Name of the solution.
+     * @return name of the solution.
+     */
+    public String name();
+
     /**
      * Solve the N-Queens problem.
      * @param size size of the board.
