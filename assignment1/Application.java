dd7929c43a2550489ccd46ff80cd24f8919691ca
revsic <revsic99@gmail.com>
2019-10-01 16:29:56 +0900

    application: Add annotation

--- assignment1/Application.java
+++ assignment1/Application.java
@@ -7,7 +7,15 @@ import problem.nqueens.App;
 import problem.nqueens.Solution;
 
 
+/**
+ * Application for showing assignment1 solutions of N-Queens problem.
+ */
 public class Application {
+    /**
+     * Main application, run BFS, DFS, DFID with given board size and log path.
+     * @param args command line argument, board size and log path.
+     * @exception java.lang.NumberFormatException exception can occur in Integer.parseInt.
+     */
     public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("usage: [size of board: INT] [path: STRING]");
