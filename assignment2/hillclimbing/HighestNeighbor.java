ac2ef0d3a3d190570fb1ff92b50574cf1e6e02df
revsic <revsic99@gmail.com>
2019-10-02 22:09:25 +0900

    highest neighbor: Add annotation

--- assignment2/hillclimbing/HighestNeighbor.java
+++ assignment2/hillclimbing/HighestNeighbor.java
@@ -1,7 +1,15 @@
 package assignment2.hillclimbing;
 
 
+/**
+ * Select highest objectives from neighbors.
+ */
 public class HighestNeighbor extends DefaultObjective {
+    /**
+     * Get neighbor which have highest objectives.
+     * @param record current record.
+     * @return neighbor record.
+     */
     @Override
     public Record next(Record record) {
         Record highest = null;
