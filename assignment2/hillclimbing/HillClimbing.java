5889f7f6474ab01638acd55f848284465ad4ae2e
revsic <revsic99@gmail.com>
2019-10-02 16:58:31 +0900

    hill climbing: Impl method solve

--- assignment2/hillclimbing/HillClimbing.java
+++ assignment2/hillclimbing/HillClimbing.java
@@ -1,6 +1,7 @@
 package assignment2.hillclimbing;
 
-import assignment2.hillclimbing.Objective;;
+import assignment2.hillclimbing.Objective;
+import problem.nqueens.NQueens;
 import problem.nqueens.Solution;
 
 
@@ -8,12 +9,12 @@ import problem.nqueens.Solution;
  * N-Queens problem solver with Hill-Climbing policy.
  */
 public class HillClimbing implements Solution {
-    public HillClimbing() {
-        
-    }
+    private int maxRetry;
+    private Objective objective;
 
-    public HillClimbing(Objective obj) {
-
+    public HillClimbing(int maxRetry, Objective objective) {
+        this.maxRetry = maxRetry;
+        this.objective = objective;
     }
 
     /**
@@ -32,6 +33,28 @@ public class HillClimbing implements Solution {
      */
     @Override
     public int[][] solve(int size) {
-        return null;
+        int try = 0;
+        Record record = null;
+        do {
+            record = Record.random(size, objective);
+            double current = objective.score(record.nQueens);
+            while (true) {
+                Record best = record.highestNeighbor();
+                double next = objective.score(best.nQueens);
+                if (current >= next) {
+                    break;
+                }
+
+                record = best;
+                current = next;
+            }
+
+            try += 1;
+        } while (record.nQueens.isSolved() || try > maxRetry);
+
+        if (try > maxRetry) {
+            return null;
+        }
+        return record.coords.toArray(new int[2][size]);
     }
 }
