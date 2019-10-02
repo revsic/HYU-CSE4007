9291344eace8431e5f099d729f809142e6d8c6ad
revsic <revsic99@gmail.com>
2019-10-02 22:05:12 +0900

    policy: Add annotation

--- assignment2/hillclimbing/Policy.java
+++ assignment2/hillclimbing/Policy.java
@@ -3,8 +3,21 @@ package assignment2.hillclimbing;
 import problem.nqueens.NQueens;
 
 
+/**
+ * Hill climbing policy.
+ */
 public interface Policy {
+    /**
+     * Compute objectives from N-Queens environment.
+     * @param queens N-Queens environment.
+     * @return objective value.
+     */
     public double objective(NQueens queens);
 
+    /**
+     * Get next records from neighbor sequence.
+     * @param current current record.
+     * @return next record.
+     */
     public Record next(Record current);
 }
