78fc756b569729908f3294258692af1950aa1f91
revsic <revsic99@gmail.com>
2019-10-16 12:46:18 +0900

    nqueens solver: Add documentation

--- assignment3/NQueensSolver.java
+++ assignment3/NQueensSolver.java
@@ -6,28 +6,57 @@ import assignment3.genetic.GeneticSolver;
 import problem.nqueens.Solution;
 
 
+/**
+ * N-Queens solver with genetic algorithm.
+ */
 public class NQueensSolver extends GeneticSolver<NQueensState> implements Solution {
+    /**
+     * Construct N-Queens solver.
+     * @param size size of the board.
+     */
     public NQueensSolver(int size) {
         super(new NQueensGene(size, 7), 5000, 500, 4500, 0);
     }
 
+    /**
+     * Name of the solution.
+     * @return name of the solution.
+     */
+    @Override
     public String name() {
         return "Genetic Algorithm";
     }
 
+    /**
+     * Implement ending condition.
+     * @param generation NQueensState[], one generation.
+     * @return NQueensState[], found solution.
+     */
+    @Override
     public NQueensState[] findSolution(NQueensState[] generation) {
         ArrayList<NQueensState> sol = new ArrayList<NQueensState>();
         for (NQueensState state : generation) {
+            // if state solved
             if (state.make().isSolved()) {
                 sol.add(state);
             }
         }
 
-        return sol.toArray(new NQueensState[sol.size()]);
+        if (sol.size() > 0) {
+            // return as array
+            return sol.toArray(new NQueensState[sol.size()]);
+        }
+        return null;
     }
 
     public static final int MAX_ITER = 500;
 
+    /**
+     * Solve the problem.
+     * @param size size of the board.
+     * @return int[][], found solution.
+     */
+    @Override
     public int[][] solve(int size) {
         NQueensState[] res = run(MAX_ITER);
         if (res != null) {
