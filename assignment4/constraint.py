ce173b79fce06675821b24e1328ed84329f49f85
revsic <revsic99@gmail.com>
2019-11-04 01:36:19 +0900

    nqueens opt: Add comments

--- assignment4/constraint.py
+++ assignment4/constraint.py
@@ -5,33 +5,53 @@ import z3
 
 
 def naive_constraint(x, num_queens):
+    """Naive constraint, from nQueensNaive for performance comparison.
+    Args:
+        x: List[List[z3.Int]], symbols.
+        num_queens: int, the number of queens.
+    Returns:
+        List[z3.BoolRef], constraints.
+    """
+    # domain constraint
     domain = [z3.Or(x[row][col] == 0, x[row][col] == 1)
               for col in range(num_queens)
               for row in range(num_queens)]
 
+    # row and column constraint
     row_constraint = [z3.Sum(x[row]) == 1 for row in range(num_queens)]
     col_constraint = [z3.Sum([x[row][col] for row in range(num_queens)]) == 1
                       for col in range(num_queens)]
+    # O(N^2) diagonal constraint
     diagonal_constraint = \
         [z3.Implies(z3.And(x[i][j] == 1, x[k][h] == 1, i != k, j != h),
                     abs(k - i) != abs(j - h))
          for i in range(num_queens) for j in range(num_queens)
          for k in range(num_queens) for h in range(num_queens)]
-
+    # sum up
     constraint = domain + row_constraint + col_constraint + diagonal_constraint
     return constraint
 
 
 def default_constraint(x, num_queens):
+    """Default N-Queens constraint.
+    Args:
+        x: List[List[z3.Int]], symbols.
+        num_queens: int, the number of queens.
+    Returns:
+        List[z3.BoolRef], constrains.
+    """
+    # domain constraint
     domain = [z3.Or(x[row][col] == 0, x[row][col] == 1)
               for col in range(num_queens)
               for row in range(num_queens)]
 
+    # clusters
     rows = [[] for _ in range(num_queens)]
     cols = [[] for _ in range(num_queens)]
     rightdown = [[] for _ in range(num_queens * 2 - 1)]
     leftdown = [[] for _ in range(num_queens * 2 - 1)]
 
+    # variable clustering
     for i in range(num_queens):
         for j in range(num_queens):
             var = x[i][j]
@@ -48,14 +68,17 @@ def default_constraint(x, num_queens):
             else:
                 leftdown[i + j].append(x[i][j])
 
+    # add constraints to each clusters
     row_const = [z3.Sum(row) == 1 for row in rows]
     col_const = [z3.Sum(col) == 1 for col in cols]
     rightdown_const = [z3.Sum(rd) <= 1 for rd in rightdown]
     leftdown_const = [z3.Sum(ld) <= 1 for ld in leftdown]
 
+    # constraint for the number of queens
     nqueens = [x[i][j] for j in range(num_queens) for i in range(num_queens)]
     nqueens = [z3.Sum(nqueens) == num_queens]
 
+    # sum up
     const = domain                          \
         + nqueens                           \
         + row_const + col_const             \
@@ -64,25 +87,47 @@ def default_constraint(x, num_queens):
 
 
 def compute_neighbor(x, y, num_queens):
+    """Generate neighbor set.
+    Args:
+        x: int, x-position.
+        y: int, y-position.
+        num_queens: int, the number of queens.
+    Returns:
+        List[Tuple[int, int]], coordinates set of neighbors.
+    """
+    # all neighbor
     # deltas = [[-1, -1], [ 0, -1], [ 1, -1],
     #           [-1,  0],           [ 1,  0],
     #           [-1,  1], [ 0,  1], [ 1,  1]]
+
+    # horizontal-vertical neighbor
     deltas = [          [ 0, -1],
               [-1,  0],           [ 1,  0],
                         [ 0,  1]]
+
+    # diagonal neighbor
     # deltas = [[-1, -1], [ 1, -1],
     #           [-1,  1], [ 1,  1]]
+
     neighbor = []
     for dx, dy in deltas:
         nx = x + dx
         ny = y + dy
+        # coordinate validation
         if 0 <= nx and nx < num_queens and 0 <= ny and ny < num_queens:
             neighbor.append([nx, ny])
     return neighbor
 
 
 def skipped_neighbor(size):
+    """Generate skipped neighbor set.
+    Args:
+        size: int, number of neighbors to skip.
+    Returns:
+        Callable, neighbor generator.
+    """
     def compute(x, y, num_queens):
+        # horizontal-vertical neighbor
         deltas = [          [ 0, -1],
                   [-1,  0],           [ 1,  0],
                             [ 0,  1]]
@@ -91,6 +136,7 @@ def skipped_neighbor(size):
             for s in range(1, size + 1):
                 nx = x + dx * s
                 ny = y + dy * s
+                # range validation
                 if 0 <= nx and nx < num_queens and 0 <= ny and ny < num_queens:
                     neighbor.append([nx, ny])
         return neighbor
@@ -98,6 +144,14 @@ def skipped_neighbor(size):
 
 
 def neighbor_heuristic(x, num_queens, neighbor_fn):
+    """Deterministic neighbor heuristic (in-and version).
+    Args:
+        x: List[List[z3.Int]], symbols.
+        num_queens: int, the number of queens.
+        neighbor_fn: Callable, neighbor generator.
+    Returns:
+        List[z3.BoolRef], constraints.
+    """
     const = []
     for i in range(num_queens):
         for j in range(num_queens):
@@ -110,6 +164,14 @@ def neighbor_heuristic(x, num_queens, neighbor_fn):
 
 
 def random_neighbor_heuristic(x, num_queens, neighbor_fn):
+    """Stochastic neighbor heuristic (in-and version).
+    Args:
+        x: List[List[z3.Int]], symbols.
+        num_queens: int, the number of queens.
+        neighbor_fn: Callable, neighbor generator.
+    Returns:
+        List[z3.BoolRef], constraints.
+    """
     const = []
     for i in range(num_queens):
         px = random.randint(0, num_queens - 1)
@@ -123,8 +185,17 @@ def random_neighbor_heuristic(x, num_queens, neighbor_fn):
 
 
 def heuristic(x, num_queens):
+    """Deprecated, do not use heuristic.
+    """
     return []
 
 
 def custom_constraint(x, num_queens):
+    """Customized constraint. 
+    Args:
+        x: List[List[z3.Int]], symbols.
+        num_queens: int, the number of queens.
+    Returns:
+        List[z3.BoolRef], constraints.
+    """
     return default_constraint(x, num_queens) + heuristic(x, num_queens)
--- assignment4/nQueensOpt.py
+++ assignment4/nQueensOpt.py
@@ -7,6 +7,12 @@ import constraint
 
 
 def timer(func):
+    """Decorator for checking time performance.
+    Args:
+        func: Callable, something callable.
+    Returns:
+        Callable, decorated function
+    """
     def decorated(*args, **kwargs):
         start = time.time()
         retn = func(*args, **kwargs)
@@ -16,14 +22,25 @@ def timer(func):
 
 @timer
 def solve(num_queens, const_fn):
+    """Solve N-Queens problem with given constraint.
+    Args:
+        num_queens: int, number of queens, N.
+        const_fn: Callable, constraint generator.
+    Returns:
+        List[int], index list where queens exist.
+    """
+    # symbol def
     x = [[z3.Int(f'x_{row}_{col}')
          for col in range(num_queens)]
          for row in range(num_queens)]
+    # generate constraint
     const = const_fn(x, num_queens)
 
+    # solve SAT problem
     s = z3.Solver()
     s.add(const)
 
+    # if solved
     if s.check() == z3.sat:
         m = s.model()
         r = [[j + 1 for j in range(num_queens) if m.evaluate(x[i][j]) == 1][0]
@@ -34,17 +51,26 @@ def solve(num_queens, const_fn):
 
 
 def main(argv):
+    """Main
+    Args:
+        argv: List[str], commandline arguments.
+    Returns:
+        int, program exit status
+    """
     num_queens = int(input('N: '))
 
+    # constraint setting
     if len(argv) > 1 and argv[1] == 'naive':
         const_fn = constraint.naive_constraint
     elif len(argv) > 1 and argv[1] == 'default':
         const_fn = constraint.default_constraint
     else:
         const_fn = constraint.custom_constraint
-    
+
+    # solve n-queens
     result, time = solve(num_queens, const_fn)
 
+    # print result
     print(result)
     print('elapsed time: {} sec'.format(time))
     return 0
