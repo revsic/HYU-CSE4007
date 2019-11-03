cf73a64553ce09a06cccd80a94a34789aee5365e
revsic <revsic99@gmail.com>
2019-11-03 19:34:00 +0900

    nqueens: Impl solve method

--- assignment4/nqueens.py
+++ assignment4/nqueens.py
@@ -3,6 +3,8 @@ import time
 
 import z3
 
+import constraint
+
 
 def timer(func):
     def decorated(*args, **kwargs):
@@ -14,11 +16,25 @@ def timer(func):
 
 @timer
 def solve(num_queens):
-    pass
+    x = [[z3.Int(f'x_{row}_{col}') for col in range(num_queens)]
+         for row in range(num_queens)]
+
+    const = constraint.naive_constraint(x, num_queens)
+
+    s = z3.Solver()
+    s.add(const)
+
+    if s.check() == z3.sat:
+        m = s.model()
+        r = [[j + 1 for j in range(num_queens) if m.evaluate(x[i][j]) == 1][0]
+             for i in range(num_queens)]
+        return r
+
+    return []
 
 
 def main(_):
-    num_queens = input('N: ')
+    num_queens = int(input('N: '))
     result, time = solve(num_queens)
 
     print(result)
